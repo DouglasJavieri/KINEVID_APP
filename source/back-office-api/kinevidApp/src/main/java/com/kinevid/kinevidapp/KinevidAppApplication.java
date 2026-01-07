b25133607e4679a5f9810736366e14aee5b65427
Douglas Javieri <douvan.22@gmail.com>
2026-01-07 00:02:48 -0400

    Initial monorepo estructura con Angular 13 y Spring Boot

---
+++ source/back-office-api/kinevidApp/src/main/java/com/kinevid/kinevidapp/KinevidAppApplication.java
@@ -0,0 +1,13 @@
+package com.kinevid.kinevidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinevidAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinevidAppApplication.class, args);
+    }
+
+}
---
+++ source/back-office-api/kinevidApp/src/test/java/com/kinevid/kinevidapp/KinevidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinevid.kinevidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinevidAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
